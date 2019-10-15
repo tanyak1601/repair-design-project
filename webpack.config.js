0daa1d36b6fe1edb150149a44d769832872b7568
tanyak1601 <tanyakk1601@gmail.com>
2019-10-15 12:26:26 +0300

    feat:add a complete project

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
           },
         ],
       },
-      { test: /\.(woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' },\
+      { test: /\.(woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
     ]
   },
   plugins: [
